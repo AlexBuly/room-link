0fb54c5da3b9f399476c5c973dd9b2c85f16f23a
AlexBuly <asbuly@icloud.com>
2024-02-27 14:53:42 -0500

    Add form for log in

--- roomlink/src/components/Login.js
+++ roomlink/src/components/Login.js
@@ -1,6 +1,21 @@
 const Login = () => {
     return (
-    < h1>Log in</h1>    
+     <div className="login">
+        <fieldset>
+        < h1>Log in</h1> 
+            <form action="post">
+                <div className="username">
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username"/>
+                </div>
+                <br />
+                <div className="password">
+                    <label htmlFor="pwd">Password:</label>
+                    <input type="password" id="pwd" name="pwd"/>
+                </div>
+            </form>
+        </fieldset> 
+    </div>
     )
 }
 
