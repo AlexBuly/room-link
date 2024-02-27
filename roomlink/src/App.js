a33952e87a3325eaa1515d2574e8e5dbb6332377
AlexBuly <asbuly@icloud.com>
2024-02-27 15:54:39 -0500

    Add media query

--- roomlink/src/App.js
+++ roomlink/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Footer from './components/Footer';
 import './components/Style.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/login" element={<Login/>}/>
           <Route path="/SignUp" element={<SignUp/>}/>
         </Routes>
+        <Footer/>
       </Router>
     </div>
   );
