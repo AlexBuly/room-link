8049409157c6e95363b4d7c3599f8213c803a51f
AlexBuly <asbuly@icloud.com>
2024-03-15 17:28:36 -0400

    Add layout for home page

--- roomlink/src/components/Home.js
+++ roomlink/src/components/Home.js
@@ -1,8 +1,21 @@
 const Home = () => {
     return (
         <div className="home">
-            
-    
+            <h1>RoomLink</h1>
+            <div className="hero"></div>
+            <div className="container mt-5">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h2>Our mission</h2>
+                        <p>What our purpose is </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>About</h3>
+                        <p>About us.</p>
+                    </div>
+                </div>
+            </div>
+           <h1>Get Started!</h1>
         </div>
     )
 }
