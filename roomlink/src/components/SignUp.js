77592c9b3d11b800ad60eaa4e0064b641ef7b65a
AlexBuly <asbuly@icloud.com>
2024-03-15 14:23:23 -0400

    Add user data with mongodb

--- roomlink/src/components/SignUp.js
+++ roomlink/src/components/SignUp.js
@@ -1,7 +1,21 @@
 const SignUp = () => {
     return (
         <div className="sign-up">
-            <h1>Create Account</h1>
+            <fieldset>
+            <h1>Create Account</h1> 
+            <form action="post">
+                <div className="username">
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username"/>
+                </div>
+                <br />
+                <div className="password">
+                    <label htmlFor="pwd">Password:</label>
+                    <input type="password" id="pwd" name="pwd"/>
+                </div>
+                <button type="sumbit">Create</button>
+            </form>
+        </fieldset> 
         </div>
     )
 }
