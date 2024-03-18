ed90c65fefc0cc9acdd8636c0512899897765914
AlexBuly <asbuly@icloud.com>
2024-03-18 17:38:01 -0400

    Edit home page

--- roomlink/src/components/Home.js
+++ roomlink/src/components/Home.js
@@ -1,22 +1,32 @@
 const Home = () => {
     return (
         <div className="home">
-            <h1>RoomLink</h1>
-            <div className="hero"></div>
-            <div className="container mt-5">
-                <div class="row">
-                    <div class="col-md-8">
-                        <h2>Our mission</h2>
-                        <p>What our purpose is </p>
-                    </div>
-                    <div class="col-md-4">
-                        <h3>About</h3>
-                        <p>About us.</p>
-                    </div>
-                </div>
-            </div>
-           <h1>Get Started!</h1>
+            <header>
+                <h1 className="nav-logo">RoomLink</h1>
+            </header>
+            <main>
+                 <section>
+                 <h2 className="match">Match easily</h2>
+                        <div className="card">
+                            <div className="card-body">
+                                <p className="card-text">Create Account</p>
+                                <p className="card-text">Fill out the survey</p>
+                                <p className="card-text">Connect with matches</p>
+                            </div>
+                            <div className="imgs">
+                                <div className="img"></div>
+                                <div className="img"></div>
+                                <div className="img"></div>
+                            </div>
+                        </div>
+                        <div className="container">
+                            <h3>Our goals</h3>
+                            <h3>See what the hype is about!</h3>
+                        </div>
+                    </section>
+            </main>
         </div>
+        
     );
 };
 
